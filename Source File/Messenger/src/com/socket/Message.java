77da3009dc539456f6e7ea367db1135364a58911
po95 <sourav.integrated95@gmail.com>
2017-12-04 19:12:27 +0600

    first commit

---
+++ Source File/Messenger/src/com/socket/Message.java
@@ -0,0 +1,23 @@
+package com.socket;
+/*
+Author: Sourav Sarker
+Registration Number: 2014331039
+CSE SUST
+*/
+
+import java.io.Serializable;
+
+public class Message implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    public String type, sender, content, recipient;
+    
+    public Message(String type, String sender, String content, String recipient){
+        this.type = type; this.sender = sender; this.content = content; this.recipient = recipient;
+    }
+    
+    @Override
+    public String toString(){
+        return "{type='"+type+"', sender='"+sender+"', content='"+content+"', recipient='"+recipient+"'}";
+    }
+}
